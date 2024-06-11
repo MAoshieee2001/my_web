fe7ff224b81c572eb11cf16d5bb702c2c42aebca
mmarint <moisesusat2001@gmail.com>
2024-06-11 12:11:18 -0500

    paginacion django y datatable

--- core/pos/static/js/product/form.js
+++ core/pos/static/js/product/form.js
@@ -1,5 +1,10 @@
+let input_serice;
+
 $(function () {
-    // * Cargamos nuestro plugins de select2
+    // Capturamos el elemento servicio
+    input_serice = $('input[name="is_service"]');
+
+    //  Cargamos nuestro plugins de select2
     $('#id_category').select2({
         theme: 'bootstrap4',
         placeholder: 'Seleccione la categoria del producto.',
@@ -24,13 +29,17 @@ $(function () {
         }
     });
 
-    // * Validamos los campos de entradass
+    //  Validamos los campos de entradass
     $('input[name="code"]').on('keypress', function (evt) {
         return validate_text_box(evt, 'numbers_letters');
     });
+
+    //  Validamos los campos de entradass
     $('input[name="names"]').on('keypress', function (evt) {
         return validate_text_box(evt, 'letters');
     });
+
+    //  Validamos los campos de entradass
     $('input[name="purchase"]').TouchSpin({
         min: 0,
         max: 10000,
@@ -40,6 +49,8 @@ $(function () {
     }).on('keypress', function (evt) {
         return validate_text_box(evt, 'decimals');
     });
+
+    //  Validamos los campos de entradass
     $('input[name="pvp"]').TouchSpin({
         min: 0,
         max: 10000,
@@ -49,6 +60,8 @@ $(function () {
     }).on('keypress', function (evt) {
         return validate_text_box(evt, 'decimals');
     });
+
+    //  Validamos los campos de entradass
     $('input[name="stock"]').TouchSpin({
         min: 0,
         max: 10000,
@@ -56,4 +69,22 @@ $(function () {
     }).on('keypress', function (evt) {
         return validate_text_box(evt, 'numbers');
     });
+
+    // Validamoss para que se oculte y visualize la informacion
+    input_serice.change(function (evt) {
+        // Capturamos los elementos que vamoss a que ocultarlo
+        let container = $('.form-group');
+        let container_purchase = container[5];
+        let container_stock = container[7];
+        // mostramos los container
+        container_purchase.style.display = 'block';
+        container_stock.style.display = 'block';
+        // Validamos si el usuario selecciono el compontenete
+        if (this.checked) {
+            container_purchase.style.display = 'none';
+            container_stock.style.display = 'none';
+        }
+    });
+
+    input_serice.trigger('change');
 });
